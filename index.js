50d34248e8a019ae2b7339a82ea83a1aadb13283
olasrum <mursalo@gmail.com>
2022-07-22 21:12:38 +1000

    Done with assignment

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  let result;
+  if (value < 400) {
+    result = 'This one is on me!';
+  } else if (value > 2000 && value < 2500) {
+    result = 'I will gladly take your thirty bucks.';
+  } else if (value > 2500) {
+    result = 'No can do.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let comment;
+  city === 'NYC' ? (comment = 'Ok, sounds good.') : (comment = 'No go.');
+  return comment;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let response;
+  switch (tip) {
+    case 'generous':
+        response = 'Thank you so much.';
+        break;
+    case 'not as generous':
+        response = 'Thank you.';
+        break;
+    default:
+        response = 'Bye.';
+        break;
+  }
+  return response;
 }
